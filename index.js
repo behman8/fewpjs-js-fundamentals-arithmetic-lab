3ccbf667123b0fc7e6000a99b7a644038223fa33
Branden <brandenehman45@gmail.com>
2021-05-22 10:50:13 -0600

    commit tree 4df7623990f051fce77b1ced542e86b9e372b500

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let num1 = 2;
-let num2 = 31;
-let multiply = num1 * num2;
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
 let random = Math.floor(Math.random() * 10) + 1;
 Number.isInteger(random);
